4214dd7e1e6e4a98f8f2d3042d807a4dedb10e6f
Kory33 <6561358+kory33@users.noreply.github.com>
2018-02-13 21:30:07 +0800

    defines TodoItem Model and relation to TodoList

---
+++ app/models/TodoItem.scala
@@ -0,0 +1,25 @@
+package models
+
+import scalikejdbc.WrappedResultSet
+import skinny.orm.{Alias, SkinnyCRUDMapper}
+
+case class TodoItem(id: Long,
+                    todoListId: Long, todoList: Option[TodoList] = Option.empty,
+                    title: String,
+                    description: String)
+
+object TodoItem extends SkinnyCRUDMapper[TodoItem] {
+  override def defaultAlias: Alias[TodoItem] = createAlias("ti")
+
+  hasOne[TodoList](
+    TodoList,
+    (todoItem, todoList) => todoItem.copy(todoList = todoList)
+  ).byDefault
+
+  override def extract(rs: WrappedResultSet, n: scalikejdbc.ResultName[TodoItem]): TodoItem = new TodoItem(
+    id = rs.get(n.id),
+    todoListId = rs.get(n.todoListId),
+    title = rs.get(n.title),
+    description = rs.get(n.description)
+  )
+}
--- app/models/TodoList.scala
+++ app/models/TodoList.scala
@@ -1,18 +1,24 @@
 package models
 
 import scalikejdbc.WrappedResultSet
+import skinny.orm.feature.associations.HasManyAssociation
 import skinny.orm.{Alias, SkinnyCRUDMapperWithId}
 
-case class TodoList(id: String, title: String)
+case class TodoList(id: String, title: String, todoItems: Seq[TodoItem] = Nil)
 
 object TodoList extends SkinnyCRUDMapperWithId[String, TodoList] {
   override def useExternalIdGenerator = true
   override def generateId: String = java.util.UUID.randomUUID.toString
-
   override def idToRawValue(id: String): String = id
   override def rawValueToId(value: Any): String = value.toString
 
   override lazy val defaultAlias: Alias[TodoList] = createAlias("tl")
+
+  lazy val todoItemsRef: HasManyAssociation[TodoList] = hasMany[TodoItem](
+    many = TodoItem -> TodoItem.defaultAlias,
+    on = (list, item) => scalikejdbc.sqls.eq(list.id, item.id),
+    merge = (list, items) => list.copy(todoItems = items)
+  )
 
   override def extract(rs: WrappedResultSet, n: scalikejdbc.ResultName[TodoList]): TodoList = new TodoList(
     id    = rs.get(n.id),
