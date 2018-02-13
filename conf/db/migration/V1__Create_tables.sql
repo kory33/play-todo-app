c2e321d2e2c0d8247c4c4ae7bb9d207470bc4549
Kory33 <6561358+kory33@users.noreply.github.com>
2018-02-13 16:24:25 +0800

    defines table structures in migration file

--- conf/db/migration/V1__Create_tables.sql
+++ conf/db/migration/V1__Create_tables.sql
@@ -1,4 +1,23 @@
 create table todo_list (
   id varchar(36) not null primary key,
   title varchar(60) not null
-)
+);
+
+create table todo_item (
+  id bigint auto_increment not null primary key,
+  todo_list_id varchar(36) not null,
+  title varchar(60) not null,
+  content text
+);
+
+create table tag (
+  id bigint auto_increment not null primary key,
+  name varchar(35),
+  color char(6)
+);
+
+create table_reply (
+  id bigint auto_increment not null primary key,
+  todo_item_id bigint not null,
+  content text
+);
