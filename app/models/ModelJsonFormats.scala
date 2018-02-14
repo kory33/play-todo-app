a4dcaa13644993cc59196994e06438fbee0add25
Kory33 <6561358+kory33@users.noreply.github.com>
2018-02-14 14:12:50 +0800

    creates generic function to convert Option[Seq[T]] to Json

--- app/models/ModelJsonFormats.scala
+++ app/models/ModelJsonFormats.scala
@@ -4,22 +4,23 @@ import play.api.libs.json.{JsValue, Json, Writes}
 
 object ModelJsonFormats {
 
+  private def optionSeqToJson[T: Writes](attribute: String, seq: Option[Seq[T]]) = seq.map { s =>
+    Json.obj(attribute -> Json.toJson(s))
+  }.getOrElse(Json.obj())
+
   implicit val todoItemWrites: Writes[TodoItem] = new Writes[TodoItem] {
     override def writes(item: TodoItem): JsValue = Json.obj(
       "id" -> item.id,
       "title" -> item.title,
       "description" -> item.description
-    ) ++
-      item.tags.map { seq => Json.obj("tags" -> Json.toJson(seq)) }.getOrElse(Json.obj())
+    ) ++ optionSeqToJson("tags", item.tags)
   }
 
   implicit val todoListWrites: Writes[TodoList] = new Writes[TodoList] {
     override def writes(list: TodoList): JsValue = Json.obj(
       "id" -> list.id,
       "title" -> list.title
-    ) ++
-      list.todoItems.map { seq => Json.obj("todo_items" -> Json.toJson(seq)) }.getOrElse(Json.obj()) ++
-      list.tags.map { seq => Json.obj("tags" -> Json.toJson(seq)) }.getOrElse(Json.obj())
+    ) ++ optionSeqToJson("todo_items", list.todoItems) ++ optionSeqToJson("tags", list.tags)
   }
 
   implicit val tagWrites: Writes[Tag] = new Writes[Tag] {
