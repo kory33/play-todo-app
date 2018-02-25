6100ca967086c0b2144c5dd48e8da5eeb972e202
Kory33 <6561358+kory33@users.noreply.github.com>
2018-02-25 08:34:42 +0800

    configures static routing to the asset

---
+++ app/controllers/AssetController.scala
@@ -0,0 +1,18 @@
+package controllers
+
+import javax.inject.{Inject, Singleton}
+
+import akka.actor.ActorSystem
+import play.api.mvc.Controller
+
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class AssetController @Inject()(actorSystem: ActorSystem)(implicit exec: ExecutionContext) extends Controller {
+  def at(path: String, indexSuffix: String, file: String) = {
+    // Append index suffix at the end of paths if terminating with /
+    val filePaths = if (file.endsWith("/")) file ++ indexSuffix else file
+
+    controllers.Assets.at(path, filePaths.stripPrefix("/"))
+  }
+}
