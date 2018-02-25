e869e1dedb410f1f9cb9302e4b3bb33cda4ccf4a
Kory33 <6561358+kory33@users.noreply.github.com>
2018-02-25 22:11:05 +0800

    fixes AssetController to work with empty(root) path

--- app/controllers/AssetController.scala
+++ app/controllers/AssetController.scala
@@ -10,8 +10,8 @@ import scala.concurrent.ExecutionContext
 @Singleton
 class AssetController @Inject()(actorSystem: ActorSystem)(implicit exec: ExecutionContext) extends Controller {
   def at(path: String, indexSuffix: String, file: String) = {
-    // Append index suffix at the end of paths if terminating with /
-    val filePaths = if (file.endsWith("/")) file ++ indexSuffix else file
+    // Append index suffix at the end of paths if terminating with / or empty
+    val filePaths = if (file.endsWith("/") || file.isEmpty) file ++ indexSuffix else file
 
     controllers.Assets.at(path, filePaths.stripPrefix("/"))
   }
