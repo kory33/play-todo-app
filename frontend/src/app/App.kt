fd484a96f2c3d71554fe06ef7cd381b4b2c03519
Kory33 <6561358+kory33@users.noreply.github.com>
2018-02-25 22:45:14 +0800

    removes react-kotlin default files and apps

--- frontend/src/app/App.kt
+++ frontend/src/app/App.kt
@@ -2,24 +2,13 @@ package app
 
 import react.*
 import react.dom.*
-import logo.*
-import ticker.*
 
 class App : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
         div("App-header") {
-            logo()
             h2 {
-                +"Welcome to React with Kotlin"
+                +"Header test"
             }
-        }
-        p("App-intro") {
-            +"To get started, edit "
-            code { +"app/App.kt" }
-            +" and save to reload."
-        }
-        p("App-ticker") {
-            ticker()
         }
     }
 }
