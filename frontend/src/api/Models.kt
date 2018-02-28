536f193b016ab36cf74d83d4ecd9b62fdf47d6f2
Kory33 <6561358+kory33@users.noreply.github.com>
2018-03-01 06:12:44 +0800

    lets models implement RProps

--- frontend/src/api/Models.kt
+++ frontend/src/api/Models.kt
@@ -1,10 +1,12 @@
 package api
 
-data class Reply(val id: Long, val content: String)
+import react.RProps
 
-data class ItemTag(val id: Long, val name: String, val color: String)
+data class Reply(val id: Long, val content: String): RProps
 
-data class TodoItem(val id: Long, val title: String, val description: String, val tags: List<ItemTag>?, val replies: List<Reply>)
+data class ItemTag(val id: Long, val name: String, val color: String): RProps
 
-data class TodoList(val id: String, val title: String, val todoItems: List<TodoItem>?, val tags: List<ItemTag>?)
+data class TodoItem(val id: Long, val title: String, val description: String, val tags: List<ItemTag>?, val replies: List<Reply>): RProps
+
+data class TodoList(val id: String, val title: String, val todoItems: List<TodoItem>?, val tags: List<ItemTag>?): RProps
 
