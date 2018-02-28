12ffa92660ecc961fba0a9fb1109cd003bc37769
Kory33 <6561358+kory33@users.noreply.github.com>
2018-03-01 06:34:00 +0800

    redefine api return value(wrap with promise)

--- frontend/src/api/Api.kt
+++ frontend/src/api/Api.kt
@@ -1,16 +1,18 @@
 package api
 
+import kotlin.js.Promise
+
 /**
  * REST API wrapper for the play-todo-app.
  */
 interface ApiWrapper {
 
-    fun createTodoList(title: String): TodoList
+    fun createTodoList(title: String): Promise<TodoList?>
 
-    fun fetchTodoList(id: String): TodoList
+    fun fetchTodoList(id: String): Promise<TodoList?>
 
-    fun createTodoItem(todoList: TodoList, title: String): TodoItem
+    fun createTodoItem(todoList: TodoList, title: String): Promise<TodoItem?>
 
-    fun getTodoItems(list: TodoList): List<TodoItem>
+    fun getTodoItems(list: TodoList): Promise<List<TodoItem>?>
 
 }
