1f01ab7875f1409a2fafc0d247f40cf3ab5acbd0
Kory33 <6561358+kory33@users.noreply.github.com>
2018-03-01 06:29:24 +0800

    redefine models

--- frontend/src/api/Models.kt
+++ frontend/src/api/Models.kt
@@ -2,11 +2,9 @@ package api
 
 import react.RProps
 
-data class Reply(val id: Long, val content: String): RProps
-
 data class ItemTag(val id: Long, val name: String, val color: String): RProps
 
-data class TodoItem(val id: Long, val title: String, val description: String, val tags: List<ItemTag>?, val replies: List<Reply>): RProps
+data class TodoItem(val id: Long, val title: String, val description: String): RProps
 
-data class TodoList(val id: String, val title: String, val todoItems: List<TodoItem>?, val tags: List<ItemTag>?): RProps
+data class TodoList(val id: String, val title: String): RProps
 
