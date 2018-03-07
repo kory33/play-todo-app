4ae4ef4f0b068ca536a99abdd143aa8a8c8b640e
Kory33 <6561358+kory33@users.noreply.github.com>
2018-03-08 01:18:52 +0800

    defines api model

---
+++ frontend/src/api/models.tsx
@@ -0,0 +1,7 @@
+class TodoItem {
+    constructor(readonly id: string, readonly title: string, readonly description: string) {}
+}
+
+class TodoList {
+    constructor(readonly id: String, readonly title: string) {}
+}
