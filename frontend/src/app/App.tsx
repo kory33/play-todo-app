359b9c3596b8e46991dede778b7ab264f6cb697a
Kory33 <6561358+kory33@users.noreply.github.com>
2018-03-10 22:18:54 +0800

    refactors toolbar into separate class

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -5,7 +5,8 @@ import { AppState } from './Store';
 import CircularProgress from 'material-ui/CircularProgress';
 import { TodoItemCreateBox } from './TodoItemCreateBox';
 import { TodoItemBox } from './TodoItemBox';
-import { Toolbar, ToolbarGroup, Dialog, RaisedButton } from 'material-ui';
+import { Dialog } from 'material-ui';
+import AppToolbar from './components/AppToolbar';
 
 const LoadingScreen = () => (
   <div className="loading-screen">
@@ -24,14 +25,7 @@ export default class App extends React.Component<{ appState: AppState }, {}> {
 
     return (
       <div className="app">
-        <Toolbar className="app-header">
-          <ToolbarGroup firstChild={true}>
-            {state.todoList.title}
-          </ToolbarGroup>
-          <ToolbarGroup>
-            <RaisedButton label="Create Todo" primary={true} onClick={() => state.showTodoItemCreationDialog = true} />
-          </ToolbarGroup>
-        </Toolbar>
+        <AppToolbar appState={state}/>
         <Dialog
           title="Create Todo Item"
           actions={[]}
---
+++ frontend/src/app/components/AppToolbar.tsx
@@ -0,0 +1,24 @@
+import { AppState } from '../Store';
+import * as React from 'react';
+import { Toolbar, ToolbarGroup, RaisedButton } from 'material-ui';
+
+export default class AppToolbar extends React.Component<{ appState: AppState }, {}> {
+  render() {
+    const state = this.props.appState;
+
+    if (state.todoList === null) {
+      throw Error('Todo list is null.');
+    }
+
+    return (
+      <Toolbar className="app-header">
+        <ToolbarGroup firstChild={true}>
+          {state.todoList.title}
+        </ToolbarGroup>
+        <ToolbarGroup>
+          <RaisedButton label="Create Todo" primary={true} onClick={() => state.showTodoItemCreationDialog = true} />
+        </ToolbarGroup>
+      </Toolbar>
+    );
+  }
+}
