5b44465b8b585f10e1479f148ae1b1e1ef2ce1d7
Kory33 <6561358+kory33@users.noreply.github.com>
2018-03-16 16:36:51 +0800

    fixes line spacings

--- app/utils/OptionUtil.scala
+++ app/utils/OptionUtil.scala
@@ -3,9 +3,7 @@ package utils
 object OptionUtil {
 
   implicit class OptionProjection[L](private val x: Option[L]) extends AnyVal {
-
     def projectLeftWith[R](right: => R): Either.LeftProjection[L, R] = x.toLeft(right).left
-
   }
 
 }
